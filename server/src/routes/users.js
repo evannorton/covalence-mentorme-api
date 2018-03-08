bff0f423cc6e32fcb6e743cca862d62fd593435f
Evan Norton <evanan39@yahoo.com>
2018-03-08 16:10:24 -0600

    AWS

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -3,33 +3,41 @@ import Table from '../table';
 import { tokenMiddleware, isLoggedIn } from '../middleware/auth.mw';
 import { generateHash } from '../utils/security';
 
-// import AWS from 'aws-sdk';
-// import multer from 'multer';
-// import multerS3 from 'multer-s3';
+import AWS from 'aws-sdk';
+import multer from 'multer';
+import multerS3 from 'multer-s3';
 
 let router = Router();
 let users = new Table('Users');
 
-// AWS.config.update({
-//     secretAccessKey: AWS_ACCESS_KEY_ID,
-//     accessKeyId: AWS_SECRET_ACCESS_KEY,
-//     region: 'us-east-2'
-// });
+let env = process.env;
 
-// let s3 = new AWS.S3();
+if (!env.AWS_ACCESS_KEY_ID) {
+    env = require('../config/s3config').default;
+}
 
-// let upload = multer({
-//     storage: multerS3({
-//         s3,
-//         bucket: S3_BUCKET_NAME,
-//         metadata: function (req, file, cb) {
-//             cb(null, { fildName: file.fieldname });
-//         },
-//         key: function (req, file, cb) {
-//             cb(null, Date.now() + '.jpg')
-//         }
-//     })
-// });
+console.log(env.AWS_ACCESS_KEY_ID);
+
+AWS.config.update({
+    secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: env.AWS_ACCESS_KEY_ID,
+    region: 'us-east-2'
+});
+
+let s3 = new AWS.S3();
+
+let upload = multer({
+    storage: multerS3({
+        s3,
+        bucket: env.S3_BUCKET_NAME,
+        metadata: function (req, file, cb) {
+            cb(null, { fildName: file.fieldname });
+        },
+        key: function (req, file, cb) {
+            cb(null, Date.now() + '.jpg')
+        }
+    })
+});
 
 router.get('/me', tokenMiddleware, isLoggedIn, (req, res) => {
     delete req.user.hash;
@@ -107,18 +115,19 @@ router.put('/:id', (req, res) => {
         });
 });
 
-// router.put('/images/:id', upload.single('image'), (req, res, next) => {
-//     let id = req.params.userid;
-//     let uri = req.file.location;
+router.put('/images/:userid', upload.single('image'), (req, res, next) => {
+    let id = req.params.userid;
+    let uri = req.file.location;
+    console.log(uri);
 
-//     users.update(id, { image: uri })
-//         .then(() => {
-//             res.sendStatus(200);
-//         }).catch((err) => {
-//             console.log(err);
-//             res.sendStatus(500);
-//         });
+    users.update(id, { image: uri })
+        .then(() => {
+            res.sendStatus(200);
+        }).catch((err) => {
+            console.log(err);
+            res.sendStatus(500);
+        });
 
-// });
+});
 
 export default router;
