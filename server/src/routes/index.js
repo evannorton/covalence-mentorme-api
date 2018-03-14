363b555fe44b0e18639f4ac651addfedb3948965
Benjamin Leavitt <wordsforben@gmail.com>
2018-03-14 09:40:41 -0500

    added stripe templates

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -6,15 +6,18 @@ import usersRouter from './users';
 import mentorSubjectsRouter from './mentorSubjects';
 import mentorSkillsRouter from './mentorSkills';
 import imagesRouter from './images';
+import stripeDonationsRouter from './stripeDonations';
 
 let router = Router();
 
 router.use('/auth', authRouter);
+router.use('/donate', stripeDonationsRouter);
 
 router.use('/users', usersRouter);
 
 router.use('/mentorSubjects', mentorSubjectsRouter);
 router.use('/mentorSkills', mentorSkillsRouter);
 router.use('/images', imagesRouter);
+router.use('/donate', donateRouter);
 
 export default router;
