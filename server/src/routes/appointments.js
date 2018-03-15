b8d1031ac6128034b3300b7e38818e6469e373f1
Evan Norton <evanan39@yahoo.com>
2018-03-15 10:10:30 -0500

    get appointments by user

--- server/src/routes/appointments.js
+++ server/src/routes/appointments.js
@@ -4,6 +4,28 @@ import Table from '../table';
 let router = Router();
 let appointments = new Table('Appointments');
 
+router.get('/mentors/:mentorid/:confirmed', (req, res) => {
+    let mentorid = req.params.mentorid;
+    let confirmed = req.params.confirmed;
+    appointments.find({ mentorid, confirmed })
+        .then((appointments) => {
+            res.send(appointments);
+        }).catch((err) => {
+            console.log(err);
+        });
+});
+
+router.get('/students/:studentid/:confirmed', (req, res) => {
+    let studentid = req.params.studentid;
+    let confirmed = req.parms.confirmed;
+    appointments.find({ studentid, confirmed })
+        .then((appointments) => {
+            res.send(appointments);
+        }).catch((err) => {
+            console.log(err);
+        });
+});
+
 router.post('/', (req, res) => {
     let appointment = req.body;
     appointments.insert(appointment)
