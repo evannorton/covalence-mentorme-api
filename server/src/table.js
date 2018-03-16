024299237c478c0dff2fff9669f17f8bdadb2bcd
Dustin Pierson <dustinapierson@gmail.com>
2018-03-16 09:58:20 -0500

    added sp's to table.js

--- server/src/table.js
+++ server/src/table.js
@@ -79,6 +79,14 @@ class Table {
         let sql = `CALL spDeleteMentorSkill(${userid}, ${skillid})`;
         return executeQuery(sql);
     }
+    spMentorAgenda(userid){
+        let sql = `Call spMentorAgenda(${userid})`;
+        return executeQuery(sql);
+    }
+    spstudentAgenda(userid){
+        let sql = `Call spStudentAgenda(${userid})`;
+        return executeQuery(sql);
+    }
 
 }
 
